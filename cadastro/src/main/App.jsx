50da58cf92bb121a66e97ac56fb8aaba7ecb2916
RafaelHSLemes <84418988+RafaelHSLemes@users.noreply.github.com>
2022-08-17 17:26:30 -0300

    Update App.jsx

--- cadastro/src/main/App.jsx
+++ cadastro/src/main/App.jsx
@@ -1,5 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import "./App.css";
-
 import Logo from "../components/templates/logo";
 import Nav from "../components/templates/nav";
 import Main from "../components/templates/main";
@@ -7,10 +8,11 @@ import Footer from "../components/templates/footer";
 
 function App() {
   return (
-    <div className="">
+    <div className="app">
       <Logo />
       <Nav />
-      <Main />
+      <Main icon="home" title="Inicio" 
+            subtitle="frete expresso"/>
       <Footer />
     </div>
   );

67be9ebd69e5ae0e58a4b2417a9c4aad25965c29
RafaelHSLemes <84418988+RafaelHSLemes@users.noreply.github.com>
2022-08-19 01:08:01 -0300

    Add files via upload

---
+++ cadastro/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import main from "../templates/main";
+
+const Main = props => {
+    <Main icon="home" title="Início">
+        <div className='display-4'>Bem-Vindo!</div>
+        <hr />
+        <p className='mb-0'>Precisando de frete? Cadastre-se agora!</p>
+    </Main>
+}
+
+export default main

0c9e4bff0b27ad0cfb548355e29b02f78daaeade
RafaelHSLemes <84418988+RafaelHSLemes@users.noreply.github.com>
2022-08-21 22:43:17 -0300

    Update App.jsx

--- cadastro/src/main/App.jsx
+++ cadastro/src/main/App.jsx
@@ -3,16 +3,15 @@ import 'font-awesome/css/font-awesome.min.css'
 import "./App.css";
 import Logo from "../components/templates/logo";
 import Nav from "../components/templates/nav";
-import Main from "../components/templates/main";
+import Home from "../components/home/Home";
 import Footer from "../components/templates/footer";
 
-function App() {
+const App = props => {
   return (
     <div className="app">
       <Logo />
       <Nav />
-      <Main icon="home" title="Inicio" 
-            subtitle="frete expresso"/>
+      <Home />
       <Footer />
     </div>
   );

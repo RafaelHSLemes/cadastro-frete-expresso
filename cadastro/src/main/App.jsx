26a147a51961bc6206879c5e18b4d9182a304e3f
RafaelHSLemes <84418988+RafaelHSLemes@users.noreply.github.com>
2022-08-29 17:54:37 -0300

    Update App.jsx

--- cadastro/src/main/App.jsx
+++ cadastro/src/main/App.jsx
@@ -1,20 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import "./App.css";
-import Logo from "../components/templates/logo";
-import Nav from "../components/templates/nav";
-import Home from "../components/home/Home";
+import React from 'react';
+
+import { BrowserRouter } from 'react-router-dom'
+
+import Logo from "../components/templates/Logo";
+import Nav from "../components/templates/Nav";
+import Routes from './Routes';
 import Footer from "../components/templates/footer";
 
-const App = props => {
-  return (
+export default props =>
+  <BrowserRouter>
     <div className="app">
       <Logo />
       <Nav />
-      <Home />
+      <Routes />
       <Footer />
     </div>
-  );
-}
-
-export default App;
+  </BrowserRouter>
